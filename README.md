dcc921804aede040dae7ad3458bf07bbe4dc9fbc
LeiZhang <isleizhang@outlook.com>
2022-07-01 21:04:25 +0800

    (FEATURE): Add LFBO optimizer

--- README.md
+++ README.md
@@ -31,29 +31,30 @@ This project is now supported by PengCheng Lab.
 
 We provide these black box optimization algorithms as follows:
 
-|    Search Algorithm    | Docs  | Official Links | multi-fideility | transfer | multi-obj |
-| :--------------------: | :---: | :------------: | :-------------: | :------: | :-------: |
-|         Random         |       |                |                 |          |           |
-|          BORE          |       |                |                 |          |           |
-|         Anneal         |       |                |                 |          |           |
-| Diffenential Evolution |       |                |                 |          |           |
-|         CMA-ES         |       |                |                 |          |           |
-|        NSGA-II         |       |                |                 |          |     √     |
-|     Regularized EA     |       |                |                 |          |           |
-|          PBT           |       |                |                 |          |           |
-|         TuRBO          |       |                |                 |          |           |
-|         LaMCTS         |       |                |                 |          |           |
-|       HyperBand        |       |                |        √        |          |           |
-|          BOHB          |       |                |        √        |          |           |
-|          DEHB          |       |                |        √        |          |           |
-|        MFES-BO         |       |                |        √        |          |           |
-|         TST-R          |       |                |                 |    √     |           |
-|          TAF           |       |                |                 |    √     |           |
-|       TAF(RGPE)        |       |                |                 |    √     |           |
-|         RMoGP          |       |                |                 |    √     |           |
-|       RGPE(mean)       |       |                |                 |    √     |           |
-|          PSO           |       |                |                 |          |           |
-|         XNES           |       |                |                 |          |           |
+|    Search Algorithm    | Docs  |                 Official Links                  | multi-fideility | transfer | multi-obj |
+| :--------------------: | :---: | :---------------------------------------------: | :-------------: | :------: | :-------: |
+|         Random         |       |                                                 |                 |          |           |
+|          BORE          |       |                                                 |                 |          |           |
+|         Anneal         |       |                                                 |                 |          |           |
+| Diffenential Evolution |       |                                                 |                 |          |           |
+|         CMA-ES         |       |                                                 |                 |          |           |
+|        NSGA-II         |       |                                                 |                 |          |     √     |
+|     Regularized EA     |       |                                                 |                 |          |           |
+|          PBT           |       |                                                 |                 |          |           |
+|         TuRBO          |       |                                                 |                 |          |           |
+|         LaMCTS         |       |                                                 |                 |          |           |
+|       HyperBand        |       |                                                 |        √        |          |           |
+|          BOHB          |       |                                                 |        √        |          |           |
+|          DEHB          |       |                                                 |        √        |          |           |
+|        MFES-BO         |       |                                                 |        √        |          |           |
+|         TST-R          |       |                                                 |                 |    √     |           |
+|          TAF           |       |                                                 |                 |    √     |           |
+|       TAF(RGPE)        |       |                                                 |                 |    √     |           |
+|         RMoGP          |       |                                                 |                 |    √     |           |
+|       RGPE(mean)       |       |                                                 |                 |    √     |           |
+|          PSO           |       |                                                 |                 |          |           |
+|          XNES          |       |                                                 |                 |          |           |
+|          LFBO          |       | [lfbo-ml/lfbo](https://github.com/lfbo-ml/lfbo) |                 |          |           |
 
 # Links
 
--- xbbo/search_algorithm/lfbo_optimizer.py
+++ xbbo/search_algorithm/lfbo_optimizer.py
@@ -3,6 +3,9 @@ import numpy as np
 from scipy.optimize import Bounds
 from scipy.optimize import minimize, OptimizeResult
 from sklearn.ensemble import RandomForestClassifier
+from xbbo.core.constants import MAXINT
+
+
 from xbbo.core.trials import Trial, Trials
 from xbbo.initial_design import ALL_avaliable_design
 
@@ -13,10 +16,10 @@ from xbbo.configspace.space import DenseConfiguration
 logger = logging.getLogger(__name__)
 
 
-@alg_register.register('bore')
-class BORE(AbstractOptimizer):
+@alg_register.register('lfbo')
+class LFBO(AbstractOptimizer):
     '''
-    ref: https://github.com/ltiao/bore
+    ref: https://github.com/lfbo-ml/lfbo
     '''
     def __init__(self,
                  space,
@@ -36,12 +39,12 @@ class BORE(AbstractOptimizer):
                                    **kwargs)
 
         # self.multi_start = multi_start(minimizer_fn=minimize)
-        self.classifier = Classfify(classify=classify)
         if self.space.get_conditions():
             raise NotImplementedError(
                 "BORE optimizer currently does not support conditional space!")
 
         self.dimension = self.space.get_dimensions()
+        self.classifier = Classfify(classify=classify, dim=self.dimension,rng=self.rng)
         bounds = self.space.get_bounds()
         self.bounds = Bounds(bounds.lb, bounds.ub)  #(bounds.lb, bounds.ub)
         self.initial_design = ALL_avaliable_design[initial_design](
@@ -51,7 +54,7 @@ class BORE(AbstractOptimizer):
         self.initial_design_configs = self.initial_design.select_configurations(
         )
 
-        self.trials = Trials(space,dim=self.dimension)
+        self.trials = Trials(space, dim=self.dimension)
         self.random_rate = random_rate
         self.num_starts = kwargs.get("num_starts", 5)
         self.num_samples = kwargs.get("num_samples", 1024)
@@ -74,47 +77,18 @@ class BORE(AbstractOptimizer):
                 self.initial_design_configs[dataset_size:dataset_size +
                                             n_suggestions]
             ]
-        # config_random = []
-        # while len(config_random) < n_suggestions:
-        #     config = self.space.sample_configuration(1)[0]
-        #     if not self.trials.is_contain(config):
-        #         config_random.append(config)
-
-        # # epsilon-greedy exploration
-        # if self.random_rate is not None and \
-        #         self.rng.binomial(p=self.random_rate, n=1):
-        #     logger.info("[Glob. maximum: skipped "
-        #                 f"(prob={self.random_rate:.2f})] "
-        #                 "Suggesting random candidate ...")
-        #     return [
-        #         Trial(configuration=config,
-        #               config_dict=config.get_dictionary(),
-        #               array=config.get_array(sparse=False),
-        #               origin='Random') for config in config_random
-        #     ]
 
         # targets: historical y-values
         targets = self.trials.get_history()[0]
-        # tau is the gamma-th quantile
-        tau = np.quantile(targets, q=self.quantile)
-        # classify historical y-values
-        z = np.less(targets, tau)
-        # update classifier
-        self.classifier.fit(self.trials.get_array(), z)
-        
-        # # Create classifier (if retraining from scratch every iteration)
-        # self._maybe_create_classifier()
-        #
-        # # Train classifier
-        # self._update_classifier()
 
-        # Scipy optimization using random x-values
-        # (The best point obtained with the historical x value is still around the historical value)
-        # (historical x-values can be supplemented as random)
+        X, Y, W = self._make_clf_data(self.trials.get_array(), targets)
+
+        # update classifier
+        self.classifier.fit(X, Y, W)
+
         X_init = self.rng.uniform(low=self.bounds.lb,
                                   high=self.bounds.ub,
-                                  size=(self.num_samples,
-                                        self.dimension))
+                                  size=(self.num_samples, self.dimension))
         f_init = self.classifier.predict(X_init)  # to minimize
 
         #Calculate the minimum value of the fitted function
@@ -149,8 +123,7 @@ class BORE(AbstractOptimizer):
             best_config = None
             for res in results:
                 if (res.success or res.status == 1) and res.fun < best_v:
-                    config = DenseConfiguration.from_array(
-                        self.space, res.x)
+                    config = DenseConfiguration.from_array(self.space, res.x)
                     if not self.trials.is_contain(config):
                         best_config = config
             assert best_config is not None
@@ -174,21 +147,61 @@ class BORE(AbstractOptimizer):
             logger.warn("Duplicate detected! Skipping...")
         return not is_duplicate
 
+    def _make_clf_data(self, X, Y, eta=1.0):
+        '''
+        Uility default use EI version(eta = 1.0)
+        when eta == 0, i.e. PI version
+        '''
+        X = np.asarray(X)
+        Y = np.asarray(Y)
+        tau = np.quantile(Y, self.quantile)
+
+        z = np.less(Y, tau)
+        x1, y1 = X[z], z[z]
+        x0, y0 = X, np.zeros_like(z)
+        w1 = (tau - Y)[z]  # Utility
+        w1 = w1**eta / np.mean(w1)  # scale constant
+        w0 = 1 - y0
+        s1 = x1.shape[0]
+        s0 = x0.shape[0]
+
+        X = np.concatenate([x1, x0], axis=0)
+        Y = np.concatenate([y1, y0], axis=0)
+        W = np.concatenate([w1 * (s1 + s0) / s1, w0 * (s1 + s0) / s0],
+                           axis=0)  # 正负样本数量均衡
+        W = W / W.mean()
+        return X, Y, W
+
 
 class Classfify():
-    def __init__(self, classify: str = 'rf'):
+    def __init__(self, classify: str = 'rf', dim=0, rng=np.random.RandomState()):
+        self.classify = classify
         if classify == 'rf':
-            self.model = RandomForestClassifier(n_estimators=25)
+            self.model = RFClassify(n_estimators=1000,
+                                                min_samples_split=2,random_state=rng)
+        elif classify == 'xgb':
+            XGBClassify = _load_class('XGBClassify')
+            self.model = XGBClassify(objective='binary:logistic',
+                                        min_child_weight=1,
+                                        learning_rate=0.3,
+                                n_estimators=100,random_state=rng)
+        elif classify == 'mlp':
+            SequentialNN = _load_class('SequentialNN')
+            self.model = SequentialNN(dim,random_state=rng
+                )
         else:
             raise NotImplementedError()
 
-    def fit(self, X, z):
-        self.model.fit(X, z.ravel())
+    def fit(self, X, z, w=None):
+        z = z.ravel()
+        if w is None:
+            w = np.ones_like(z, dtype='float')
+        self.model.fit(X, z, sample_weight=w)
 
     def predict(self, x):
         if x.ndim == 1:
             x = x.reshape(1, -1)
-        return 1 - self.model.predict_proba(x)[:, 1]
+        return 1 - self.model.predict_proba(x)
 
 
 def from_bounds(bounds):
@@ -206,4 +219,81 @@ def from_bounds(bounds):
     return (low, high), dim
 
 
-opt_class = BORE
+class RFClassify(RandomForestClassifier):
+    def predict_proba(self,*args, **kwargs):
+        return super().predict_proba(*args, **kwargs)[:,-1]
+
+def _load_class(classname='XGBClassify'):
+    if classname == 'XGBClassify':
+        from xgboost import XGBClassifier
+        class XGBClassify(XGBClassifier):
+            def predict_proba(self,*args, **kwargs):
+                return super().predict_proba(*args, **kwargs)[:,-1]
+        return XGBClassify
+    elif classname == 'SequentialNN':
+        import torch.nn as nn
+        import torch
+        from torch.utils.data import TensorDataset, DataLoader
+        class Network(nn.Module):
+            def __init__(self,input_dim,
+                output_dim=1,
+                num_layers=2,
+                num_units=32,) -> None:
+                super().__init__()
+                self.layers = nn.ModuleList()
+
+                for i in range(num_layers):
+                    if not i:
+                        self.layers.append(nn.Linear(input_dim, num_units))
+                        self.layers.append(nn.ReLU())
+                    self.layers.append(nn.Linear(num_units, num_units))
+                    self.layers.append(nn.ReLU())
+
+                self.layers.append(nn.Linear(num_units, output_dim))
+            
+            def forward(self, x):
+                for layer in self.layers:
+                    x = layer(x)
+                return x
+            
+        class SequentialNN():
+            def __init__(self,input_dim,
+                output_dim=1,
+                num_layers=2,
+                num_units=32,random_state=np.random.RandomState()) -> None:
+                self.input_dim = input_dim
+                self.output_dim = output_dim
+                self.num_layers = num_layers
+                self.num_units = num_units
+                seed = random_state.randint(MAXINT)
+                torch.manual_seed(seed)
+
+            def fit(self, X, z, sample_weight):
+                self.net = Network(self.input_dim,self.output_dim, self.num_layers, self.num_units)
+                optimizer = torch.optim.Adam(self.net.parameters(), lr=1e-4, weight_decay=0.0)
+                batch_size = 64
+
+                X = torch.tensor(X, dtype=torch.float)
+                z = torch.tensor(z, dtype=torch.float).unsqueeze(-1)
+                sample_weight = torch.tensor(sample_weight, dtype=torch.float).unsqueeze(-1)
+
+                dataset = TensorDataset(X, z, sample_weight)
+                loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
+
+                for i in range(500):
+                    for x, y, w in loader:
+                        optimizer.zero_grad()
+                        y_ = self.net(x)
+                        loss = nn.BCEWithLogitsLoss(weight=w)(y_, y)
+                        loss.backward()
+                        optimizer.step()
+            
+            def predict_proba(self, x):
+                x = torch.tensor(x, dtype=torch.float)
+                with torch.no_grad():
+                    y = torch.sigmoid(self.net(x))
+                return y.squeeze().numpy()
+            
+        return SequentialNN
+
+opt_class = LFBO
